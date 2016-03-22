095ae1f2733a745e0927624a0050ad1e7a021f5a
成宽 <wencheng.hwc@alibaba-inc.com>
2016-03-22 14:47:41 +0800

    fix

--- DEV.md
+++ DEV.md
@@ -1,6 +1,20 @@
 
 # TODO
 
+今天 
+
+- 如果file，自动读取，然后配上fileType
+- remote，请求自身的value, 改为'original'
+- mtop这种有特征的，可以直接为其添加其它url映射到'original'。或者作为mockx的default配置，公司这些接口域名，直接映射到'original'
+- meta charset的问题 为何gbk不行
+- 良田的那个需求是什么?
+- 能否在映射中写判断语句
+- 案例，篡改headers中的cookie
+
+是否可能有问题
+
+- 如果两条规则，一条有data匹配，一条没data匹配，会中那一条
+
 - 在家的时候，mockx的DIP会有问题，会一直请求
 - 开启debug模式时，将转发remote请求的headers和rawBody都write到一个文件中，方便排查。
 
--- test/full-example-mockx.js
+++ test/full-example-mockx.js
@@ -2,26 +2,26 @@
 	// 基础用法
 	// =====================================
 
-	// 简单的json文件
+	// 简单的json文件 返回json数据
 	{
 		"route": "/api/userinfo",
 		"json": "userinfo.json"
 	},
 
-	// 当需要jsonp类型
+	// 当需要jsonp类型 返回自动包裹jsonp格式
 	{
 		"route": "/api/userinfo",
 		"json": "userinfo.json",
 		"jsonp": "callback"
 	},
 
-	// 简单的jsData
+	// 简单的jsData 返回jsData执行的返回值 
 	{
 		"route": "/api/userinfo",
 		"jsData": "userinfo.js"
 	},
 
-	// 简单的remote
+	// 简单的remote 返回远程remote返回的数据
 	{
 		"route": "/api/userinfo",
 		"remote": "http://dip.alibaba-inc.com/api/v2/services/schema/mock/7222"
@@ -38,23 +38,27 @@
 	// =====================================
 
 
-	// 场景1 某个专做数据接口的域名，新开的项目某个接口后端还在开发中，需要此接口走本地，其余接口都走线上
+	// 场景1 某个专做数据接口的域名，新开的项目某个接口后端还在开发中，需要此接口映射本地，其余接口都走线上
 	// 需要api.m.taobao.com 指向127.0.0.1 在 flex-hosts.json中 "127.0.0.1"下添加 "freeway.ju.daily.taobao.net" 这一项
 	{
+		// '/.*/i' 正则意为所有
 		"route": /.*/i,
 		"host": "api.m.taobao.com",
-		"remote": "self"		
+		// 'origin' 透明转发，即转发到线上的服务器
+		"remote": "origin"		
 	},
 
 	{
+		// route规则中，字符串 > 正则，因此'api.m.taobao.com/get/userinfo'会匹配到这个规则，而不是上面的正则设定
 		"route": "/get/userinfo",
 		"host": "api.m.taobao.com"
 	},
 
 	// 场景2 dip schema
-	// 注：在dip上schema填写时，必须要有meta.uri字段，即设定 接口地址
+	// 注：在dip上schema填写时，在schema源码中必须要有meta.uri字段，通过 编辑 -> meta -> 接口地址 设置
 	{
-		dipSchema: 5158
+		dipSchema: 5158,
+		responseTime: 1000
 	},
 	// 转义为
 	{
@@ -65,8 +69,50 @@
 	    "responseTime": 1000
   	},
 
+  	// 场景3 线上HTML出问题，暂时没修改vm，想直接修改线上HTML测试下问题
+  	// 映射其余到线上
+  	{
+  		"route": /.*/i,
+  		"host": "ju.taobao.com",
+  		"remote": "origin"
+  	},
+  	// https://ju.taobao.com/tg/brand_items.htm?act_sign_id=14541818&seller_id=749901026
+  	{
+  		"route": "/tg/brand_items.htm",
+  		"data": {
+  			"act_sign_id": 14541818
+  			"seller_id": 749901026
+  		},
+  		"host": "ju.taobao.com",
+  		"file": "mock/html/brand_items.htm",
+  		"fileType": "text/html"
+  	},
+
+  	// 场景4 不同的query返回不同的内容
+  	{
+  		"route": "/tg/get_good_info.htm",
+  		"data": {
+  			"itemId": -1
+  		},
+  		"json": "mock/json/get_good_info_fail.json"
+  	},
+
+  	{
+  		"route": "/tg/get_good_info.htm",
+  		"data": {
+  			"itemId": 1234567
+  		},
+  		"json": "mock/json/get_good_info_success.json"
+  	},
+
+  	// 场景5 一个list数据，不想每页的数据都是一样的
+  	{
+  		"route": "/tg/get_items.htm",
+  	}
+
+
 	// 场景3 dip app
-	// 一个集中的app，其内包含很多dip schema，也必须场景2的dip schema
+	// 一个集中的app，其内包含很多dip schema，dip schema必须遵守场景2中dip schema说明
 	{
 		dipApp: 3333
 	},
